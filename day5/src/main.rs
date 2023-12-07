3bdba4cfc1d2993bf60d4de2cf3f9849996a02d0
Chris M <christophe.mollaret@gmail.com>
2023-12-07 06:16:30 +0100

    day 5 part 2

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -21,6 +21,101 @@ fn main() {
     .unwrap();
 
     day5_part1(&text);
+    day5_part2(&text);
+}
+
+fn day5_part2(t: &str) {
+
+    let data = parse_map_sections(t);
+    let seeds = parse_seed(&data[0][0]);
+    let maps: Vec<Map> = data[1..data.len()].iter().map(parse_maps).collect();
+
+    //ordonner les maps
+    let res = maps.iter().fold(seeds.seeds, |acc, map| apply_map_to_and_split(map, acc));
+
+    //println!("display {:?}", data);
+    //println!("maps {:?}", maps);
+    println!("res {:?}", res.iter().step_by(2).min().unwrap());
+}
+
+fn apply_map_to_and_split(map: &Map, v: Vec<u64>) -> Vec<u64> {
+    let mut acc: Vec<u64> = Vec::new();
+    for i in (0..v.len()).step_by(2) {
+        let result = apply_map_to_one_seed(map, &v[i..(i+2)]);
+        acc = [acc, result].concat();
+    }
+
+    return acc;
+    
+}
+
+fn apply_map_to_one_seed(map: &Map, v: &[u64]) -> Vec<u64> {
+    let mut base = v[0];
+    let mut len = v[1];
+    let mut res: Vec<u64> = Vec::new();
+    let mut finished = false;
+
+    for i in 0..map.lines.len() {
+        if base + len < map.lines[i].src { 
+            res.push(base);
+            res.push(len);
+            finished = true;
+            break;
+        }
+        if base > map.lines[i].src + map.lines[i].len {
+            //do nothing and iter
+            continue;
+        }
+        if base + len <= map.lines[i].src + map.lines[i].len {
+             
+            //cas base < l.base
+            if base < map.lines[i].src {
+                let x = map.lines[i].src - base;
+                res.push(base);
+                res.push(x);
+                res.push(map.lines[i].dst);
+                res.push(len-x);
+            } else {
+                //cas base >= l.base
+                let x = base - map.lines[i].src;
+                res.push(map.lines[i].dst+x);
+                res.push(len);
+            }
+            finished = true;
+            break;
+        }
+        
+        if base >= map.lines[i].src {
+            
+            //cas base + x <= l.base + l.len
+            let x = map.lines[i].src+map.lines[i].len - base;
+            if base + len <= map.lines[i].src + map.lines[i].len {
+                res.push(map.lines[i].dst+(map.lines[i].len-x));
+                res.push(len);
+            } else {
+                res.push(map.lines[i].dst+(map.lines[i].len-x));
+                res.push(x);
+            }
+            
+            //cas base + x > l.base + l.len
+            if x < len {
+                base = base + x;
+                len = len - x;
+            } else {
+                finished = true;
+            }
+        }
+    }
+
+    //bigger than everything
+    if !finished {
+        res.push(base);
+        res.push(len);
+    };
+
+    //println!("{:?}", res);
+
+    return res;
 }
 
 fn day5_part1(t: &str) {
@@ -85,7 +180,8 @@ fn get_numbers(t: &str) -> Vec<u64> {
 
 fn parse_maps(m : &Vec<String>) -> Map {
 
-    let lines: Vec<Line> = m[1..m.len()].iter().map(|v| get_numbers(v)).map(|v| Line { dst: v[0], src: v[1], len: v[2]}).collect();
+    let mut lines: Vec<Line> = m[1..m.len()].iter().map(|v| get_numbers(v)).map(|v| Line { dst: v[0], src: v[1], len: v[2]}).collect();
+    lines.sort_by_key(|d| d.src);
     return Map {
         lines
     };
